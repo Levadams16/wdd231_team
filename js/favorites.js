b3dfadc68e3c60b2c3df180f649eab0226d44fa2
levadams16 <ada24047@byui.edu>
2025-11-06 11:09:11 -0700

    Added local storage functionality

---
+++ js/favorites.js
@@ -0,0 +1,8 @@
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+const favoriteMoviesSection = document.querySelector('.favoriteMovies');
+
+function displayFavorites() {
+
+}
+
+displayFavorites();
