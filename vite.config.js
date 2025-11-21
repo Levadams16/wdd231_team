a1f489961643edf17d808fd74d550f70bf5ba1d4
landonedwards <lando.s.edwards@gmail.com>
2025-11-20 19:45:08 -0700

    added config file so the subpage will show up

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // The key (main, movie) is just a name for the chunk
+        main: resolve(__dirname, 'index.html'),
+        movie: resolve(__dirname, 'movie.html'),
+      },
+    },
+  },
+});
